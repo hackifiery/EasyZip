3fd550553157e07aa2cad3a918e0a81ba069c49b
hackifiery <129475587+hackifiery@users.noreply.github.com>
2023-11-09 10:35:31 -0800

    Update test_easyzip.py

--- tests/test_easyzip.py
+++ tests/test_easyzip.py
@@ -6,7 +6,7 @@ import unittest
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 sys.path.insert(0, project_root)
 
-from easyzip.easyzip import EasyZip
+from EasyZip.easyzip import EasyZip
 class TestEasyZip(unittest.TestCase):
 
     def setUp(self):
