ea266ad2d216b3d29690e65c0e52669fcd726032
hackifiery <129475587+hackifiery@users.noreply.github.com>
2023-11-09 10:30:48 -0800

    Update test_easyzip.py

--- tests/test_easyzip.py
+++ tests/test_easyzip.py
@@ -3,7 +3,7 @@ import sys
 import unittest
 
 # Add the root directory of your project to the Python path
-project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__))
+project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 if project_root not in sys.path:
     sys.path.insert(0, project_root)
