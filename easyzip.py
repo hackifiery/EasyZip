472401779c0f563d99fdcb037b818e2a0f7f89ed
hackifiery <129475587+hackifiery@users.noreply.github.com>
2023-11-16 12:16:12 -0800

    Update easyzip.py

--- easyzip.py
+++ easyzip.py
@@ -2,16 +2,11 @@ import zipfile
 import os
 
 class EasyZip:
-    def __init__(self, zip_filename, mode='w'):
-        """
-        Constructor for EasyZip class.
-
-        Args:
-            zip_filename (str): The name of the zip file to create.
-        """
+    def __init__(self, filename, mode='w'):
+        self.filename = filename
         self.mode = mode
-        self.zip_filename = zip_filename
-        self.zip_file = zipfile.ZipFile(zip_filename, 'w', zipfile.ZIP_DEFLATED)
+        self.zipfile = None
+
     def __enter__(self):
         self.zipfile = zipfile.ZipFile(self.filename, self.mode)
         return self
@@ -20,31 +15,19 @@ class EasyZip:
         if self.zipfile:
             self.zipfile.close()
 
-    def add_file(self, file_path, arcname=None):
-        """
-        Add a file to the zip archive.
+    def add_file(self, file_path):
+        if self.zipfile:
+            self.zipfile.write(file_path, os.path.basename(file_path))
 
-        Args:
-            file_path (str): The path to the file to be added.
-            arcname (str, optional): The name of the file in the archive. 
-                If not provided, the base name of the file is used.
-        """
-        if not arcname:
-            arcname = os.path.basename(file_path)
-        self.zip_file.write(file_path, arcname)
 
     def add_directory(self, directory_path):
-        """
-        Add all files within a directory and its subdirectories to the zip archive.
+        if self.zipfile:
+            for root, dirs, files in os.walk(directory_path):
+                for file in files:
+                    file_path = os.path.join(root, file)
+                    arcname = os.path.relpath(file_path, directory_path)
+                    self.zipfile.write(file_path, arcname)
 
-        Args:
-            directory_path (str): The path to the directory to be added.
-        """
-        for root, _, files in os.walk(directory_path):
-            for file in files:
-                file_path = os.path.join(root, file)
-                arcname = os.path.relpath(file_path, directory_path)
-                self.add_file(file_path, arcname)
 
     def close(self):
         """
@@ -53,15 +36,8 @@ class EasyZip:
         self.zip_file.close()
 
     def extract_to(self, extract_path):
-        """
-        Extract the contents of the zip archive to a specified directory.
-
-        Args:
-            extract_path (str): The path where the archive contents will be extracted.
-        """
-        with zipfile.ZipFile(self.zip_filename, 'r') as zip_ref:
-            zip_ref.extractall(extract_path)
-
+        if self.zipfile:
+            self.zipfile.extractall(extract_path)
 if __name__ == "__main__":
     # Example usage
     with EasyZip('example.zip') as ez:
